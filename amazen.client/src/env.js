364e27f39057782e544ab1bc1067c4b68f491efb
boriswart <boriswart@yahoo.com>
2021-07-20 09:31:15 -0600

    Amazen initial

--- amazen.client/src/env.js
+++ amazen.client/src/env.js
@@ -1,5 +1,5 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
-export const domain = ''
-export const audience = ''
-export const clientId = ''
+export const domain = 'wanderse.us.auth0.com'
+export const audience = 'http://vacation.com/'
+export const clientId = 'FnrMgXj72Xz5FzfACk6d1RHn3D0KfC0kS'
